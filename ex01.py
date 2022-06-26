c54f4ec27c2886925b0472c84f7f40e9bdf4a858
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-26 10:54:49 -0300

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -11,21 +11,21 @@
 
 # entrada de dados
 
-l1 = []
-for i in range(10):
-  valor = int(input(f"informe o valor[{i + 1}]: "))
-  l1.append(valor)
+l1 = [] # criar uma lista vazia
+for i in range(10): # criar um laço de repetição para ler 10 valores e armazenar em uma lista
+  valor = int(input(f"informe o valor[{i + 1}]: ")) # ler o valor e armazenar em uma variável
+  l1.append(valor) # adicionar o valor na lista l1
 
 # calcular o quadrado dos elementos dessa lista, armazenando o resultado em oura lista (L2),
-l2 = list(range(len(l1)))
+l2 = list(range(len(l1))) # criar uma lista vazia para armazenar o resultado do quadrado dos elementos da lista l1
 
-for i in range(len(l1)):
-  l2[i] = l1[i] ** 2
+for i in range(len(l1)): # criar um laço para calcular o quadrado dos elementos da lista l1
+  l2[i] = l1[i] ** 2 # armazenar o resultado do quadrado dos elementos da lista l1 na lista l2
 
 # imprimir todas as listas.
-print(f"lista 1 = {l1}")
+print(f"lista 1 = {l1}") # imprimir a lista l1 com os valores lidos
 
-print("lista 2 = ", end='')
-for i in range(len(l2)):
-  print(l2[i], end=' ')
+print("lista 2 = ", end='') # imprimir a lista l2 com os valores calculados do quadrado dos elementos da lista l1
+for i in range(len(l2)): # criar um laço para imprimir os valores da lista l2
+  print(l2[i], end=' ') # imprimir os valores da lista l2 separados por espaço
 
